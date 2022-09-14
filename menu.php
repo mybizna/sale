501f8e1349ee4fe057dfa197389382bcd2abee11
Dedan Irungu <dedanirungu@gmail.com>
2022-09-14 12:18:20 +0300

    changes to menu

--- menu.php
+++ menu.php
@@ -4,15 +4,13 @@ $this->add_module_info("sale", [
     'title' => 'Sale',
     'description' => 'Sale',
     'icon' => 'fas fa-chart-pie',
-    'path' => '/transactions/sales',
+    'path' => '/sale/admin/sale',
     'class_str'=> 'text-success border-success'
 ]);
 
 
 //$this->add_menu("module", "key", "title","path", "icon", "position");
-$this->add_menu("sale", "dashboard", "Dashboard", "/dashboard", "fas fa-cogs", 1);
-$this->add_menu("sale", "sales", "Sales", "/sales", "fas fa-cogs", 5);
-$this->add_menu("sale", "purchases", "Purchases", "/purchases", "fas fa-cogs", 5);
+$this->add_menu("sale", "sales", "Sales", "/sale/admin/sale", "fas fa-cogs", 5);
 
 //$this->add_submenu("module", "key", "title","path", "position");
 
