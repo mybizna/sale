94bab97e084f18622b17dc9f44929e2c3118d967
Dedan Irungu <dedanirungu@gmail.com>
2022-09-14 12:19:48 +0300

    changes to menu

--- menu.php
+++ menu.php
@@ -11,6 +11,3 @@ $this->add_module_info("sale", [
 
 //$this->add_menu("module", "key", "title","path", "icon", "position");
 $this->add_menu("sale", "sales", "Sales", "/sale/admin/sale", "fas fa-cogs", 5);
-
-//$this->add_submenu("module", "key", "title","path", "position");
-
