e36392126d2d44d0e361c6c9d0d3e5a57eeb2270
Dedan <dedanirungu@gmail.com>
2024-04-01 18:21:31 +0300

    Delete Shipping entity class

---
+++ right.php
@@ -0,0 +1,53 @@
+<?php
+
+/** @var \Modules\Base\Classes\Fetch\Rights $this */
+
+$this->add_right("sale", "cart", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "cart", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "cart", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "cart", "staff", view:true, add:true, edit:true);
+$this->add_right("sale", "cart", "registered", view:true, add:true);
+$this->add_right("sale", "cart", "guest", view:true, );
+
+$this->add_right("sale", "cartitem", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "cartitem", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "cartitem", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "cartitem", "staff", view:true, add:true, edit:true);
+$this->add_right("sale", "cartitem", "registered", view:true, add:true);
+$this->add_right("sale", "cartitem", "guest", view:true, );
+
+$this->add_right("sale", "order", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "order", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "order", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "order", "staff", view:true, add:true, edit:true);
+$this->add_right("sale", "order", "registered", view:true, add:true);
+$this->add_right("sale", "order", "guest", view:true, );
+
+$this->add_right("sale", "orderitem", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "orderitem", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "orderitem", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "orderitem", "staff", view:true, add:true, edit:true);
+$this->add_right("sale", "orderitem", "registered", view:true, add:true);
+$this->add_right("sale", "orderitem", "guest", view:true, );
+
+$this->add_right("sale", "sale", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "sale", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "sale", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "sale", "staff", view:true, add:true, edit:true);
+$this->add_right("sale", "sale", "registered", view:true, add:true);
+$this->add_right("sale", "sale", "guest", view:true, );
+
+$this->add_right("sale", "saleitem", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "saleitem", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "saleitem", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "saleitem", "staff", view:true, add:true, edit:true);
+$this->add_right("sale", "saleitem", "registered", view:true, add:true);
+$this->add_right("sale", "saleitem", "guest", view:true, );
+
+$this->add_right("sale", "shipping", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "shipping", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "shipping", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("sale", "shipping", "staff", view:true, add:true, edit:true);
+$this->add_right("sale", "shipping", "registered", view:true, add:true);
+$this->add_right("sale", "shipping", "guest", view:true, );
+
